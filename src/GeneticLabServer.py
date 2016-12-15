eb6caf997dbdb2ad45ed7cb2e4d76ee25db3e866
Jackkell <bronzeeagle1@hotmail.com>
2016-12-14 23:10:44 -0700

    started replication for the server

--- src/GeneticLabServer.py
+++ src/GeneticLabServer.py
@@ -1,2 +1,19 @@
-class GeneticLabServer:
-    pass
+from pysyncobj import SyncObj
+from pysyncobj import replicated
+
+
+class GeneticLabServer(SyncObj):
+    def __init__(self):
+        super(GeneticLabServer, self).__init__('localhost:1337', [])
+        self.cannonResults = []
+
+    @replicated
+    def __saveResult(self, results):
+        pass
+
+    @replicated
+    def runSimulation(self):
+        pass
+
+    def run(self):
+        pass
