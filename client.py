b70906e2b0588ab0b23988e58468a3ef37a4050a
root <root@localhost.localdomain>
2016-12-16 19:56:51 -0800

    fixed the client, which was still trying to talk to localhost

--- client.py
+++ client.py
@@ -1,7 +1,10 @@
 from src.GeneticLabClient import GeneticLabClient
 
 def main():
-    geneticLabClient = GeneticLabClient(9000, "127.0.0.1")
+
+    #geneticLabClient = GeneticLabClient(9000, "127.0.0.1")
+    geneticLabClient = GeneticLabClient(9000, "172.22.71.28")
+
     geneticLabClient.run()
 
 
