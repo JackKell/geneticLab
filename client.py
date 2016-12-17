862affc61450b782e890f2215bfeb7f2d8c2766d
root <root@localhost.localdomain>
2016-12-16 21:03:32 -0800

    works with two servers, trying out using menu

--- client.py
+++ client.py
@@ -5,7 +5,8 @@ def main():
     #geneticLabClient = GeneticLabClient(9000, "127.0.0.1")
     geneticLabClient = GeneticLabClient(9000, "172.22.71.28")
 
-    geneticLabClient.run()
+    #geneticLabClient.run()
+    geneticLabClient.runExperimentMenu()
 
 
 if __name__ == "__main__":
